c90d0d56d6c85b174e525ed05dd5cfcb715aec32
Atiksh Chawla <atikshchawla@gmail.com>
2026-04-06 10:54:14 +0530

    style: format code for better readability in sign-in failure page

--- src/app/signinfail/page.js
+++ src/app/signinfail/page.js
@@ -1,13 +1,15 @@
 import LogoutButton from "@/components/logoutbutton";
 
 export default function failure() {
-    return (
-        <div className="justify-center items-center relative min-h-screen flex flex-col bg-black/50">
-            <div className="absolute top-4 right-4 z-10">
-                <LogoutButton />
-            </div>
-            <div className="absolute inset-0 -z-5 rounded-[1280px] bg-[#669C7D] blur-[375px] opacity-35"></div>
-            <h1 className="text-[70px] font-brand bg-linear-to-b from-white to-[rgba(213,213,213,0.68)] bg-clip-text text-transparent text-center px-4">Authentication Failed. <br/> You do not have the required permissions.</h1>
-        </div>
-    )
+	return (
+		<div className="justify-center items-center relative min-h-screen flex flex-col bg-black/50">
+			<div className="absolute top-4 right-4 z-10">
+				<LogoutButton />
+			</div>
+			<div className="absolute inset-0 -z-5 rounded-[1280px] bg-[#669C7D] blur-[375px] opacity-35"></div>
+			<h1 className="text-[70px] font-brand bg-linear-to-b from-white to-[rgba(213,213,213,0.68)] bg-clip-text text-transparent text-center px-4">
+				Authentication Failed. <br /> You do not have the required permissions.
+			</h1>
+		</div>
+	);
 }
